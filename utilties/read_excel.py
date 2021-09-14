ffab276d25ff76cdeea2a8c35b7a4e0463ddfdab
youyou <1604927210@qq.com>
2021-09-14 14:17:00 +0800

    update

--- utilties/read_excel.py
+++ utilties/read_excel.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
-
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+ 
 class Excel:
     def __init__(self, old_file):
         self.old_file = old_file
@@ -8,3 +11,34 @@ class Excel:
            print("check")
     def rsa_mail(self，str，key): #加密
         return rsa(str,key)
+
+def encrypt(data, password):
+    bs = AES.block_size
+    pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+    iv = Random.new().read(bs)
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    data = cipher.encrypt(pad(data))
+    data = iv + data
+    return (data)
+ 
+def decrypt(data, password):
+    bs = AES.block_size
+    if len(data) <= bs:
+        return (data)
+    unpad = lambda s : s[0:-ord(s[-1])]
+    iv = data[:bs]
+    cipher = AES.new(password, AES.MODE_CBC, iv)
+    data  = unpad(cipher.decrypt(data[bs:]))
+    return (data)
+ 
+if __name__ == '__main__':
+    data = 'd437814d9185a290af20514d9341b710'
+    password = '78f40f2c57eee727a4be179049cecf89' #16,24,32位长的密码
+    encrypt_data = encrypt(data, password)
+    encrypt_data = base64.b64encode(encrypt_data)
+    print ('encrypt_data:', encrypt_data)
+ 
+ 
+    encrypt_data = base64.b64decode(encrypt_data)
+    decrypt_data = decrypt(encrypt_data, password)
+    print ('decrypt_data:', decrypt_data)
